95caad807d846d107f422fbf8f92142ae91b0508
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-01-14 21:44:30 -0800

    wired up forms and fixed tables to create relevant data and made css changes

--- js/app.js
+++ js/app.js
@@ -38,9 +38,10 @@ function CoffeeStores(name, minHr, maxHr, cupsPerCust, poundsPerCust) {
       this.hourlyCupConversionPlusLb.push((this.hourCust[i] * this.cupsPerCust/20) + this.hourlyLbSold[i]); //stores total beans sold in the hour
     }
   };
-  // this.render = function() {
-  //   createTable();
-  // }
+  this.render = function() {
+    createTable();
+    locationBeanTotals();
+    }
 }
 var pikePlace = new CoffeeStores('Pike Place Market', 14, 55, 1.2, 3.7);
 var capHill = new CoffeeStores('Capitol Hill', 32, 48, 3.2, .4);
@@ -48,6 +49,39 @@ var spl = new CoffeeStores('Seattle Public Library', 49, 75, 2.6, .2);
 var slu = new CoffeeStores('South Lake Union', 35, 88, 1.3, 3.7);
 var seatac = new CoffeeStores('Seatac Airport', 68, 124, 1.1, 2.7);
 var webSales = new CoffeeStores('Websales', 3, 6, 0, 6.7);
+
+var userInput = document.getElementById('bean-form');
+var newNamedStore = document.getElementById('newStoreTable');
+var formData = [];
+
+function handleStoreSubmit(event) {
+  console.log(event);
+  event.preventDefault(); //prevents page reload
+  if (!event.target.where.value || !event.target.maxCustomers.value || !event.target.maxCustomers.value || !event.target.cups.value || !event.target.pounds.value) {
+    return alert('field cannot be empty!');
+  }
+
+  var name = event.target.where.value; //form data to variables
+  var minHr = parseInt(event.target.minCustomers.value);
+  var maxHr = parseInt(event.target.maxCustomers.value);
+  var cupsPerCust = parseInt(event.target.cups.value);
+  var poundsPerCust = parseInt(event.target.pounds.value);
+
+  var newStore = new CoffeeStores(name, minHr, maxHr, cupsPerCust, poundsPerCust);
+
+  formData.push(newStore);
+  newStore.listElements();
+  newStore.sumTotals();
+  places.push(newStore);
+  newStore.render();
+
+  event.target.where.value = null; //clears forms
+  event.target.minCustomers.value = null;
+  event.target.maxCustomers.value = null;
+  event.target.cups.value = null;
+  event.target.pounds.value = null;
+}
+userInput.addEventListener('submit', handleStoreSubmit); //listener
 
 pikePlace.listElements();
 capHill.listElements();
@@ -62,7 +96,32 @@ slu.sumTotals();
 seatac.sumTotals();
 webSales.sumTotals();
 
-var createTable = function () {
+var places = [pikePlace, capHill, spl, slu, seatac, webSales];
+var locationBeanTotals = function() {
+  var header = ['Location', 'Bean Total (lb)']
+  var sectEl = document.getElementById('totals');
+  var tblEl = document.createElement('table');
+  var trEl = document.createElement('tr');
+  for (var i = 0; i < header.length; i++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = header[i];
+    trEl.appendChild(thEl);
+    }
+  tblEl.appendChild(trEl);
+  for (var i = 0; i < places.length; i++){ //makes row
+    var thEl = document.createElement('th');
+    var trEl = document.createElement('tr');
+    thEl.textContent = places[i].name;
+    trEl.appendChild(thEl);
+    var tdEl = document.createElement('td');
+    tdEl.textContent = places[i].custTotal;
+    trEl.appendChild(tdEl);
+    tblEl.appendChild(trEl);
+    tblEl.appendChild(trEl);
+    sectEl.appendChild(tblEl);
+  }
+}
+var createTable = function() {
   var times = ['Location', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Totals'];
   var sectEl = document.getElementById('test');
   var tblEl = document.createElement('table');
@@ -74,7 +133,6 @@ var createTable = function () {
     trEl.appendChild(thEl);
     }
   tblEl.appendChild(trEl);
-  var places = [pikePlace, capHill, spl, slu, seatac, webSales];
 
    for (var i = 0; i < places.length; i++){ //makes row
     var thEl = document.createElement('th');
@@ -84,14 +142,15 @@ var createTable = function () {
 
     for (var j = 0; j < time.length; j++){ //makes cells
       var tdEl = document.createElement('td');
-      tdEl.textContent = places[i].hourlyCupConversionPlusLb[j].toFixed(1);
+      tdEl.textContent = places[i].hourCust[j];
       trEl.appendChild(tdEl);
     }
     var tdEl = document.createElement('td');
-    tdEl.textContent = places[i].beanPlusCups.toFixed(1);
+    tdEl.textContent = places[i].custTotal;
     trEl.appendChild(tdEl);
     tblEl.appendChild(trEl);
   }
     sectEl.appendChild(tblEl);
 }
+locationBeanTotals();
 createTable();
